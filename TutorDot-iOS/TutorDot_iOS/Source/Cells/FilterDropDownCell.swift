ba2fda4bebb8e54a849101b0ff6d3dc17d8e515e
sophryu99 <sehwa_ryu@berkeley.edu>
2020-11-08 18:29:16 +0900

    Dev

---
+++ TutorDot-iOS/TutorDot_iOS/Source/Cells/FilterDropDownCell.swift
@@ -0,0 +1,24 @@
+//
+//  FilterDropDownCell.swift
+//  TutorDot_iOS
+//
+//  Created by Sehwa Ryu on 2020/11/05.
+//  Copyright © 2020 Sehwa Ryu. All rights reserved.
+//
+
+import UIKit
+
+class FilterDropDownCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
